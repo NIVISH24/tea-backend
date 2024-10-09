ee1421e46c8a53b396d6e4a0f48bd3e990d02f67
sam-shervin <samshervin.sebastian@gmail.com>
2024-10-09 21:51:59 +0530

    Refactor LLM.py and RAG2.py for improved document querying and response generation

--- app-backend/LLM.py
+++ app-backend/LLM.py
@@ -1,25 +1,28 @@
 import ollama
+model = "llama3.2"
 
 def search_suggestions(user_input):
     prompt = f"This is the topic name: '{user_input}'. Based on this, generate a list of search suggestions that can help you refine your search query for data you can feed on to provide a more accurate response."
-    output = ollama.generate(model="llama3.2", prompt=prompt)
-    output = ollama.generate(model="llama3.2", prompt=f"{output['response']} \n\n convert this to python list of strings, (eg. [\"topic1\", \"topic2\", ...]) and nothing else, no explanations")
-    output = eval(output["response"][(output.find('[')):(output.find(']')+1)])
+    output = ollama.generate(model=model, prompt=prompt)
+    output = ollama.generate(model=model, prompt=f"{output['response']} \n\n convert this to python list of strings, (eg. [\"topic1\", \"topic2\", ...]) and nothing else, no explanations")
+    output = (output["response"])
+    output = output[output.find('['):output.find(']')+1]
     print(output)
     return(output)
 
 
 def generate_topic_name(user_input):
     prompt = f"Based on the user's input, '[{user_input}]', generate a clear and concise topic name that encapsulates the main subject. Just one or few words and nothing else."
-    output = ollama.generate(model="llama3.2", prompt=prompt)
+    output = ollama.generate(model=model, prompt=prompt)
     print("topic name: ", output["response"])
     return output['response']
 
 def generate_subheadings_and_related_topics(topic_name):
     prompt = f"For the topic '[{topic_name}]', list all the types, subheadings, and related topics necessary to fully understand this course."
-    output = ollama.generate(model="llama3.2", prompt=prompt)
-    output = ollama.generate(model="llama3.2", prompt=f"{output['response']} convert this to python list of strings and nothing else, no explanations eg:['subheading1', 'subheading2', ...]")
-    output = eval(output["response"][(output.find('[')):(output.find(']')+1)])
+    output = ollama.generate(model=model, prompt=prompt)
+    output = ollama.generate(model=model, prompt=f"{output['response']} \n\n convert this to python list of strings and nothing else, no explanations eg:['subheading1', 'subheading2', ...]")
+    output = (output["response"])
+    output = output[output.find('['):output.find(']')+1]
     return (output)  # Assuming the output is a string representation of a list.
 
 def generate_content(topic_name, region, age, interests):
@@ -28,7 +31,7 @@ def generate_content(topic_name, region, age, interests):
              f"- User Age: {age}\n" \
              f"- User Interests: {interests}\n" \
              f"Include examples relevant to the user's context."
-    output = ollama.generate(model="llama3.2", prompt=prompt)
+    output = ollama.generate(model=model, prompt=prompt)
     print(output["response"])
     return output['response']
 
--- app-backend/main.py
+++ app-backend/main.py
@@ -1,4 +1,4 @@
-from LLM import search_suggestions, generate_subheadings_and_related_topics, generate_topic_name
+from LLM import search_suggestions, generate_subheadings_and_related_topics, generate_topic_name, generate_content
 from scrape import scrape_and_save_to_pdf
 from RAG2 import query_rag
 import ollama
@@ -19,8 +19,24 @@ for query in search_queries_list:
 
 # Send to RAG to generate a response
 response = query_rag(generate_subheadings_and_related_topics(prompt))
-response = ollama.generate(model="llama3.2", prompt=f"{response} convert this to python one dimensional list of strings and nothing else, no explanations")
-print(f"Response: {response['response']}")
+response = ollama.generate(model="llama3.2", prompt=f"{response} \n\n convert this to python list of strings and nothing else, no explanations eg:['subheading1', 'subheading2', ...]")
+output = (response["response"])
+output = output[output.find('['):output.find(']')+1]
+response = eval(output)
+
+user_input = "Teach me about DBMS"
+region = "Chennai, TN, India"
+age = 19
+interests = ["singing", "dancing", "gazing through the stars", "pursuing engineering at VIT University", "President of Seraphic club - VIT Chennai (mental health club)"]
+content = [generate_content(i, region, age, interests) for i in response]
+
+# save this to separate text files
+
+for i, c in enumerate(content):
+    with open(f"{response[i]}.txt", "w") as f:
+        f.write(c)
+        print(f"Content for {response[i]} saved to {response[i]}.txt")
+        
     
 
 
